962f2831a869e48db9889cf17e12f4e18ee74d50
cdhanna <cdhanna@wpi.edu>
2014-08-03 00:27:55 -0400

    no proxy update

--- SmallNet/SmallNet/SNetProp.cs
+++ SmallNet/SmallNet/SNetProp.cs
@@ -12,7 +12,7 @@ namespace SmallNet
         //public const string DISCONNECT_NOTIFICATION = "disc";
         //public const string CREDENTIALS = "cred";
         //public const string CREATE_NEW_CLIENT_MODEL = "newmodel";
-        public const int HOST_ID = 1;
+        public const int HOST_ID = 1; //means player ID will start at 2
         public static int getPort()
         {
             return 5005;
