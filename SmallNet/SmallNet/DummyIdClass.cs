0333541657b8e2aa949fffdd3f77bb12d98907b6
timarc530 <usparatrooper52@gmail.com>
2014-08-04 17:01:01 -0400

    added dummyID class for serializer testing

---
+++ SmallNet/SmallNet/DummyIdClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmallNet
+{
+    class DummyIdClass : Id
+    {
+        int id;
+        public int Id { get { return this.id; } }
+    }
+}
