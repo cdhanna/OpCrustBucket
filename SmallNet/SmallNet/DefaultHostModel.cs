354804a52b28958a70397b8320b9a99e73902b78
cdhanna <cdhanna@wpi.edu>
2014-08-06 16:30:46 -0400

    commented defhostmodel

--- SmallNet/SmallNet/DefaultHostModel.cs
+++ SmallNet/SmallNet/DefaultHostModel.cs
@@ -8,36 +8,67 @@ using System.Net.Sockets;
 
 namespace SmallNet
 {
+    /// <summary>
+    /// The default host model that will be used on new SmallNet projects. It is recommended that new SmallNet host models use this class for their base class. 
+    /// WARNING: all implementations must have a no-arg constructor.
+    /// </summary>
+    /// <typeparam name="T">The type of client model being used with the SmallNet project</typeparam>
     abstract class DefaultHostModel<T> : HostModel<T> where T:ClientModel
     {
+        /// <summary>
+        /// A list of all the clients running on the host
+        /// </summary>
         private List<BaseClientProxy<T>> clients;
+
+        /// <summary>
+        /// A map of all client IDs on the host, pointing to the client with the same ID
+        /// </summary>
         private Dictionary<int, BaseClientProxy<T>> clientIdTable;
+        
+        /// <summary>
+        /// the number of connected clients
+        /// </summary>
         public int ClientProxyCount { get { return this.clients.Count; } }
 
-        int clientIdIncer = SNetProp.HOST_ID ;
+
+        private int clientIdIncer = SNetProp.HOST_ID ;
 
 
-        public BaseClientProxy<T> getClientProxy(int index)
-        {
-            return index > -1 && index < ClientProxyCount ? clients[index] : null;
-        }
-
+        /// <summary>
+        /// Create a new default host model
+        /// </summary>
         public DefaultHostModel()
         {
             this.clients = new List<BaseClientProxy<T>>();
             this.clientIdTable = new Dictionary<int, BaseClientProxy<T>>();
         }
 
+        /// <summary>
+        /// get the client at the given index, or null if the index is out of range
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public BaseClientProxy<T> getClientProxy(int index)
+        {
+            return index > -1 && index < ClientProxyCount ? clients[index] : null;
+        }
+
+        /// <summary>
+        /// construct a new client
+        /// </summary>
+        /// <param name="socket"></param>
+        /// <returns></returns>
         public BaseClientProxy<T> generateNewClient(Socket socket)
         {
             clientIdIncer++;
             BaseClientProxy<T> client = new BaseClientProxy<T>(socket, this, clientIdIncer);
-            this.clientIdTable[clientIdIncer] = client;
-
             return client;
         }
 
-
+        /// <summary>
+        /// add a client to the model
+        /// </summary>
+        /// <param name="client"></param>
         public void addClient(BaseClientProxy<T> client)
         {
 
@@ -49,7 +80,7 @@ namespace SmallNet
 
 
             this.clients.Add(client);
-
+            this.clientIdTable[clientIdIncer] = client;
 
             //tell the clients about the new client
             foreach (BaseClientProxy<T> proxy in this.clients)
@@ -57,18 +88,23 @@ namespace SmallNet
            
                 proxy.playerJoined(client);
             }
-
-            
-
-            
-
-
         }
+
+        /// <summary>
+        /// remove a client from the model
+        /// </summary>
+        /// <param name="client"></param>
         public void removeClient(BaseClientProxy<T> client)
         {
             this.clients.Remove(client);
+            //TODO remove map entry
         }
 
+        /// <summary>
+        /// broadcast a message to all connected clients
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="rawMessage">Should be the serialized form of the given SMessage</param>
         public void sendMessageToAll(SMessage message, String rawMessage)
         {
             if (!this.validateMessage(message))
@@ -83,12 +119,21 @@ namespace SmallNet
                 proxy.sendMessage(rawMessage);
             }
         }
+        
+        /// <summary>
+        /// broadcast a message to all connected clients
+        /// </summary>
+        /// <param name="message"></param>
         public void sendMessageToAll(SMessage message)
         {
             String rawMessage = SNetUtil.encodeMessage(message);
             this.sendMessageToAll(message, rawMessage);
         }
 
+        /// <summary>
+        /// updates the client proxies, and THEN the host.
+        /// </summary>
+        /// <param name="time"></param>
         public void update(GameTime time)
         {
             foreach (BaseClientProxy<T> proxy in this.clients)
@@ -98,8 +143,12 @@ namespace SmallNet
             this.updateHost(time);
         }
 
+        /// <summary>
+        /// called when the host is shutting down.
+        /// </summary>
         public void onShutdown()
         {
+            //send a message to all the clients, letting them know the end has come.
             SMessage msg = new Messages.DisconnectionMessage();
             foreach (BaseClientProxy<T> proxy in this.clients)
             {
@@ -107,19 +156,35 @@ namespace SmallNet
             }
         }
 
+        /// <summary>
+        /// new implementation must implement this. This is called when the host model should be updated
+        /// </summary>
+        /// <param name="time"></param>
         public abstract void updateHost(GameTime time);
+
+        /// <summary>
+        /// new implementation must define this. This is called when the model is first created.
+        /// </summary>
         public abstract void init();
 
+        /// <summary>
+        /// new implementation must define this. This is called when the host is shutting down
+        /// </summary>
         public abstract void destroy();
 
+        /// <summary>
+        /// validate a message.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns>true if the message should be allowed to propegate through the network</returns>
         public abstract bool validateMessage(SMessage message);
 
+        /// <summary>
+        /// new implementation must define this. This is called when a message is recieved
+        /// </summary>
+        /// <param name="message"></param>
         public abstract void onMessage(SMessage message);
 
-       
-        //public void sendMessage(SMessage message)
-        //{
-        //    throw new NotImplementedException();
-        //}
+
     }
 }
